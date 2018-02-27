5a05ed0d79bfc92f211f428b851b9b7fdf90505a
Armen Zambrano G <armenzg@gmail.com>
2018-02-27 13:33:54 -0500

    Bootstrap with Neutrino

---
+++ .neutrinorc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  use: [
+    '@neutrinojs/airbnb',
+    [
+      '@neutrinojs/react',
+      {
+        html: {
+          title: 'perf-goggles'
+        }
+      }
+    ]
+  ]
+};
