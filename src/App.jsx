9904c7ded3a0f442473a643701d622241123992e
Armen Zambrano G <armenzg@gmail.com>
2018-03-06 09:42:21 -0500

    Refactor: Split out App's logic into PerfherderContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,29 @@
 import { Component } from 'react';
 import './App.css';
-import Graph from './components/graph';
-import subbenchmarks from './utils/perfherder';
+import PerfherderContainer from './containers/perfherder';
+
 
 export default class App extends Component {
   state = {
-    data: undefined,
+    hasError: undefined,
   }
 
-  async componentDidMount() {
-    const { perfherderUrl, data } = await subbenchmarks({
-      suite: 'ARES6',
-      platform: 'linux64',
-    });
-    // eslint-disable-next-line
-    this.setState({ perfherderUrl, data });
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    console.log(error, info);
   }
 
   render() {
-    const { perfherderUrl, data } = this.state;
-
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
     return (
       <div className="App">
-        <a href={perfherderUrl} target="_blank">ARES6 (all subtests)</a>
-        {data && (
-          <div>
-            {Object.values(data).map(el => (
-              <div key={el.meta.test} style={{ textAlign: 'center' }}>
-                <a href={el.meta.url} target="_blank">{el.meta.test}</a>
-                <Graph
-                  key={el.meta.test}
-                  {...el}
-                  graphOptions={{
-                    x_accessor: 'datetime',
-                    y_accessor: 'value',
-                    min_y_from_data: true,
-                    full_width: true,
-                  }}
-                />
-              </div>
-            ))}
-          </div>
-        )}
+        <PerfherderContainer
+          suite="ARES6"
+          platform="linux64"
+        />
       </div>
     );
   }
---
+++ src/containers/perfherder.jsx
@@ -0,0 +1,53 @@
+import { Component } from 'react';
+import propTypes from 'prop-types';
+import Graph from '../components/graph';
+import subbenchmarks from '../utils/perfherder';
+
+export default class PerfherderContainer extends Component {
+  state = {
+    data: undefined,
+  }
+
+  async componentDidMount() {
+    const { perfherderUrl, data } = await subbenchmarks({
+      platform: this.props.platform,
+      suite: this.props.suite,
+    });
+    // eslint-disable-next-line
+    this.setState({ perfherderUrl, data });
+  }
+
+  render() {
+    const { perfherderUrl, data } = this.state;
+
+    return (
+      <div>
+        {data && (
+          <div>
+            <a href={perfherderUrl} target="_blank">{this.props.suite} (all subtests)</a>
+            {Object.values(data).map(el => (
+              <div key={el.meta.test} style={{ textAlign: 'center' }}>
+                <a href={el.meta.url} target="_blank">{el.meta.test}</a>
+                <Graph
+                  key={el.meta.test}
+                  {...el}
+                  graphOptions={{
+                    x_accessor: 'datetime',
+                    y_accessor: 'value',
+                    min_y_from_data: true,
+                    full_width: true,
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+PerfherderContainer.propTypes = {
+  platform: propTypes.string.isRequired,
+  suite: propTypes.string.isRequired,
+};
