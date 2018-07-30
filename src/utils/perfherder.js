7e3c53150c2ac1857cb829a7b91ab8ed5d07dd09
Armen Zambrano G <armenzg@gmail.com>
2018-07-30 10:58:50 -0400

    Support Raptor framework
    We had harcoded the framework ID 1 which represents Talos across the codebase.
    This change allows for passing a different framework ID.
    In our case, this allows for using Raptor's framework.

--- src/utils/perfherder.js
+++ src/utils/perfherder.js
@@ -4,7 +4,7 @@ import { stringify } from 'query-string';
 
 export const TREEHERDER = 'https://treeherder.mozilla.org';
 const PROJECT = 'mozilla-central';
-const NINENTY_DAYS = 90 * 24 * 3600;
+const DEFAULT_TIMERANGE = 14 * 24 * 3600;
 
 export const signaturesUrl = (project = PROJECT) => (
   `${TREEHERDER}/api/project/${project}/performance/signatures/`
@@ -14,29 +14,31 @@ export const dataPointsEndpointUrl = (project = PROJECT) => (
   `${TREEHERDER}/api/project/${project}/performance/data/`
 );
 
-export const subtestsPerfDataUrl = (signatureIds, project = PROJECT, interval = NINENTY_DAYS) => {
-  const url = dataPointsEndpointUrl(project);
-  const queryParams = stringify({
-    framework: 1,
-    interval,
-    signature_id: signatureIds,
-  });
-  return `${url}?${queryParams}`;
-};
+export const subtestsPerfDataUrl =
+  (frameworkId, signatureIds, project = PROJECT, interval = DEFAULT_TIMERANGE) => {
+    const url = dataPointsEndpointUrl(project);
+    const queryParams = stringify({
+      framework: frameworkId,
+      interval,
+      signature_id: signatureIds,
+    });
+    return `${url}?${queryParams}`;
+  };
 
 // The data contains an object where each key represents a subtest
 // Each data point of that subtest takes the form of:
 // {job_id: 162620134, signature_id: 1659462, id: 414057864, push_id: 306862, value: 54.89 }
-const fetchPerfDataForSubtests = async (signatureIds) => {
-  const response = await fetch(subtestsPerfDataUrl(signatureIds));
+const fetchPerfDataForSubtests = async (frameworkId, signatureIds) => {
+  const response = await fetch(subtestsPerfDataUrl(frameworkId, signatureIds));
   return response.json();
 };
 
-const perherderGraphUrl = (signatureIds, platform, project = PROJECT, timerange = NINENTY_DAYS) => {
-  let baseDataUrl = `${TREEHERDER}/perf.html#/graphs?timerange=${timerange}`;
-  baseDataUrl += `&${signatureIds.sort().map(id => `series=${project},${id},1,1`).join('&')}`;
-  return baseDataUrl;
-};
+const perherderGraphUrl =
+  (frameworkId, signatureIds, platform, project = PROJECT, timerange = DEFAULT_TIMERANGE) => {
+    let baseDataUrl = `${TREEHERDER}/perf.html#/graphs?timerange=${timerange}`;
+    baseDataUrl += `&${signatureIds.sort().map(id => `series=${project},${id},1,${frameworkId}`).join('&')}`;
+    return baseDataUrl;
+  };
 
 const queryAllTreeherderOptions = async () => {
   const response = await fetch(`${TREEHERDER}/api/optioncollectionhash/`);
@@ -62,8 +64,8 @@ const treeherderOptions = async () => {
   return transformOptionCollectionHash(optionCollectionHash);
 };
 
-const queryPlatformSignaturesNoSubtests = async (platform) => {
-  const response = await fetch(`${signaturesUrl()}?framework=1&platform=${platform}&subtests=0`);
+const queryPlatformSignaturesNoSubtests = async (frameworkId, platform) => {
+  const response = await fetch(`${signaturesUrl()}?framework=${frameworkId}&platform=${platform}&subtests=0`);
   return response.json();
 };
 
@@ -72,8 +74,8 @@ const querySubtestsAssociatedToParent = async (parentHash) => {
   return response.json();
 };
 
-const signaturesForPlatformSuite = async (platform, suite) => {
-  const allPlatformSignatures = await queryPlatformSignaturesNoSubtests(platform);
+const signaturesForPlatformSuite = async (frameworkId, platform, suite) => {
+  const allPlatformSignatures = await queryPlatformSignaturesNoSubtests(frameworkId, platform);
   const filteredSignatures = Object.keys(allPlatformSignatures)
     .reduce((res, signatureHash) => {
       const jobSignature = allPlatformSignatures[signatureHash];
@@ -89,15 +91,24 @@ const signaturesForPlatformSuite = async (platform, suite) => {
   return filteredSignatures;
 };
 
-const findParentSignatureHash = (signatures, options, option, extraOptions) => {
+const findParentSignatureHash = (
+  signatures,
+  options,
+  option = 'pgo',
+  extraOptions,
+) => {
   const result = [];
   Object.keys(signatures).forEach((hash) => {
     const signature = signatures[hash];
     const optionCollection = options[signature.option_collection_hash];
-    if (optionCollection &&
-        optionCollection.includes(option) &&
-        isEqual(signature.extra_options, extraOptions)) {
-      result.push(signature.parentSignatureHash);
+    if (optionCollection && optionCollection.includes(option)) {
+      if (extraOptions && extraOptions.length > 0) {
+        if (isEqual(signature.extra_options, extraOptions)) {
+          result.push(signature.parentSignatureHash);
+        }
+      } else {
+        result.push(signature.parentSignatureHash);
+      }
     }
   });
 
@@ -108,27 +119,27 @@ const findParentSignatureHash = (signatures, options, option, extraOptions) => {
   return result[0];
 };
 
-export const parentSignatureHash = async (platform, suite, option, extraOptions) => {
+export const parentSignatureHash = async (frameworkId, platform, suite, option, extraOptions) => {
   const [signatures, options] = await Promise.all([
-    signaturesForPlatformSuite(platform, suite, option),
+    signaturesForPlatformSuite(frameworkId, platform, suite, option),
     treeherderOptions(),
   ]);
   return findParentSignatureHash(signatures, options, option, extraOptions);
 };
 
-const prepareData = async (subtestsInfo) => {
+const prepareData = async (frameworkId, subtestsInfo) => {
   const signatureIds = Object.values(subtestsInfo).map(v => v.id);
   const data = {
     // Link to Perfherder with all subtests
-    perfherderUrl: perherderGraphUrl(signatureIds),
+    perfherderUrl: perherderGraphUrl(frameworkId, signatureIds),
     data: {},
   };
-  const dataPoints = await fetchPerfDataForSubtests(signatureIds);
+  const dataPoints = await fetchPerfDataForSubtests(frameworkId, signatureIds);
 
   Object.keys(dataPoints).forEach((subtestHash) => {
     data.data[subtestHash] = {
       meta: {
-        url: perherderGraphUrl([subtestHash]),
+        url: perherderGraphUrl(frameworkId, [subtestHash]),
         ...subtestsInfo[subtestHash], // Original object from Perfherder
       },
       data: dataPoints[subtestHash].map(datum => ({
@@ -141,13 +152,13 @@ const prepareData = async (subtestsInfo) => {
   return data;
 };
 
-export const subbenchmarksData = async (platform, suite, option, extraOptions) => {
-  const parentHash = await parentSignatureHash(platform, suite, option, extraOptions);
+export const subbenchmarksData = async (frameworkId, platform, suite, option, extraOptions) => {
+  const parentHash = await parentSignatureHash(frameworkId, platform, suite, option, extraOptions);
   if (!parentHash) {
     return {};
   }
   const subtests = await querySubtestsAssociatedToParent(parentHash);
-  return prepareData(subtests);
+  return prepareData(frameworkId, subtests);
 };
 
 export default subbenchmarksData;
--- test/utils/perfherder.test.js
+++ test/utils/perfherder.test.js
@@ -9,13 +9,14 @@ import {
 } from '../../src/utils/perfherder';
 
 const assert = require('assert');
-const LINUX64_SIGNATURES = require('../mocks/linux64SignaturesNoSubtests');
-const LINUX64_JETSTREAM_SUBTESTS = require('../mocks/linux64JetStreamSubtests');
-const LINUX64_JETSTREAM_DATA = require('../mocks/linux64JetStreamData');
-const LINUX64_JETSTREAM_EXPECTED_DATA = require('../expected/linux64JetStreamExpectedData');
-const LINUX64_DROMAEO_DOM_SUBTESTS = require('../mocks/linux64DromaeoDomSubtests');
-const LINUX64_DROMAEO_DOM_DATA = require('../mocks/linux64DromaeoDomData');
-const LINUX64_DROMAEO_DOM_EXPECTED_DATA = require('../expected/linux64DromaeoDomExpectedData');
+const LINUX64_SIGNATURES = require('../mocks/linux64/signaturesNoSubtests');
+const LINUX64_JETSTREAM_DATA = require('../mocks/linux64/JetStream/data');
+const LINUX64_JETSTREAM_SUBTESTS = require('../mocks/linux64/JetStream/subtests');
+const LINUX64_JETSTREAM_EXPECTED_DATA = require('../mocks/linux64/JetStream/expected');
+const WIN10_SIGNATURES = require('../mocks/win10/signaturesNoSubtests');
+const WIN10_MMA_SUBTESTS = require('../mocks/win10/MotionMarkAnimometer/subtests');
+const WIN10_MMA_DATA = require('../mocks/win10/MotionMarkAnimometer/data');
+const WIN10_MMA_EXPECTED_DATA = require('../mocks/win10/MotionMarkAnimometer/expected');
 const OPTION_COLLECTION_HASHES = require('../mocks/optionCollectionHash');
 
 const downcastDatetimesToStrings = (data) => {
@@ -28,55 +29,71 @@ const downcastDatetimesToStrings = (data) => {
   return newData;
 };
 
-describe('Linux64 data', () => {
-  const platform = 'linux64';
-  const extraOptions = ['e10s', 'stylo'];
-  fetchMock.get(`${signaturesUrl()}?framework=1&platform=${platform}&subtests=0`, LINUX64_SIGNATURES);
-  fetchMock.get(`${TREEHERDER}/api/optioncollectionhash/`, OPTION_COLLECTION_HASHES);
+fetchMock.get(`${TREEHERDER}/api/optioncollectionhash/`, OPTION_COLLECTION_HASHES);
 
-  describe('Linux64 Jetstream data', () => {
-    const signatureIds = [
-      1661255, 1661256, 1661257, 1661258, 1661259, 1661260, 1661261, 1661262, 1661263, 1661264,
-      1661265, 1661266, 1661267, 1661268, 1661269, 1661270, 1661271, 1661272, 1661273, 1661274,
-      1661275, 1661276, 1661277, 1661278, 1661279, 1661280, 1661281, 1661282, 1661283, 1661284,
-      1661285, 1661286, 1661287, 1661288, 1661289, 1661290, 1661291, 1661292, 1661293, 1661294,
-    ];
-    const parentHash = '46ca6eb015193051661117a30bd39e6f25ee4744';
-    fetchMock.get(`${signaturesUrl()}?parent_signature=${parentHash}`, LINUX64_JETSTREAM_SUBTESTS);
-    fetchMock.get(subtestsPerfDataUrl(signatureIds), LINUX64_JETSTREAM_DATA);
+describe('Talos', () => {
+  const frameworkId = 1;
 
-    it('should find Linux64 JetStream pgo signature hash', async () => {
-      const signature = await parentSignatureHash(platform, 'JetStream', 'pgo', extraOptions);
-      assert.equal(signature, '46ca6eb015193051661117a30bd39e6f25ee4744');
-    });
+  describe('Linux64', () => {
+    const platform = 'linux64';
+    const extraOptions = ['e10s', 'stylo'];
+    fetchMock.get(`${signaturesUrl()}?framework=${frameworkId}&platform=${platform}&subtests=0`, LINUX64_SIGNATURES);
 
-    it('should find Linux64 JetStream opt signature hash', async () => {
-      const signature = await parentSignatureHash(platform, 'JetStream', 'opt', extraOptions);
-      assert.equal(signature, '4e714a801f334b874de0aeda22df6a21b8f40500');
-    });
+    describe('Jetstream', () => {
+      const signatureIds = [
+        1661255, 1661256, 1661257, 1661258, 1661259, 1661260, 1661261, 1661262, 1661263, 1661264,
+        1661265, 1661266, 1661267, 1661268, 1661269, 1661270, 1661271, 1661272, 1661273, 1661274,
+        1661275, 1661276, 1661277, 1661278, 1661279, 1661280, 1661281, 1661282, 1661283, 1661284,
+        1661285, 1661286, 1661287, 1661288, 1661289, 1661290, 1661291, 1661292, 1661293, 1661294,
+      ];
+      const parentHash = '46ca6eb015193051661117a30bd39e6f25ee4744';
+      fetchMock.get(`${signaturesUrl()}?parent_signature=${parentHash}`, LINUX64_JETSTREAM_SUBTESTS);
+      fetchMock.get(subtestsPerfDataUrl(frameworkId, signatureIds), LINUX64_JETSTREAM_DATA);
 
-    it('should not find Linux64 JetStream debug signature hash', async () => {
-      const signature = await parentSignatureHash(platform, 'JetStream', 'debug', extraOptions);
-      assert.equal(signature, undefined);
-    });
+      it('should find Linux64 JetStream pgo signature hash', async () => {
+        const signature = await parentSignatureHash(frameworkId, platform, 'JetStream', 'pgo', extraOptions);
+        assert.equal(signature, '46ca6eb015193051661117a30bd39e6f25ee4744');
+      });
 
-    it('should find Linux64 JetStream pgo subtests data', async () => {
-      const data = await subbenchmarksData(platform, 'JetStream', 'pgo', extraOptions);
-      const modifiedExpectedData = downcastDatetimesToStrings(LINUX64_JETSTREAM_EXPECTED_DATA);
-      assert.deepEqual(data, modifiedExpectedData);
+      it('should find Linux64 JetStream opt signature hash', async () => {
+        const signature = await parentSignatureHash(frameworkId, platform, 'JetStream', 'opt', extraOptions);
+        assert.equal(signature, '4e714a801f334b874de0aeda22df6a21b8f40500');
+      });
+
+      it('should not find Linux64 JetStream debug signature hash', async () => {
+        const signature = await parentSignatureHash(frameworkId, platform, 'JetStream', 'debug', extraOptions);
+        assert.equal(signature, undefined);
+      });
+
+      it('should find Linux64 JetStream pgo subtests data', async () => {
+        const data = await subbenchmarksData(frameworkId, platform, 'JetStream', 'pgo', extraOptions);
+        const modifiedExpectedData = downcastDatetimesToStrings(LINUX64_JETSTREAM_EXPECTED_DATA);
+        assert.deepEqual(data, modifiedExpectedData);
+      });
     });
   });
+});
 
-  describe('Linux64 DromaeoDom data', () => {
-    const signatureIds = [1651446, 1651447, 1651448, 1651449];
-    const parentHash = '947870e091eef0257755ce0e6fd6302e1704c15b';
-    fetchMock.get(`${signaturesUrl()}?parent_signature=${parentHash}`, LINUX64_DROMAEO_DOM_SUBTESTS);
-    fetchMock.get(subtestsPerfDataUrl(signatureIds), LINUX64_DROMAEO_DOM_DATA);
+describe('Raptor', () => {
+  const frameworkId = 10;
 
-    it('should find Linux64 Dromaeo DOM pgo subtests data', async () => {
-      const data = await subbenchmarksData(platform, 'dromaeo_dom', 'pgo', extraOptions);
-      const modifiedExpectedData = downcastDatetimesToStrings(LINUX64_DROMAEO_DOM_EXPECTED_DATA);
-      assert.deepEqual(data, modifiedExpectedData);
+  describe('Windows 10', () => {
+    const platform = 'windows10-64';
+    fetchMock.get(`${signaturesUrl()}?framework=${frameworkId}&platform=${platform}&subtests=0`, WIN10_SIGNATURES);
+
+    describe('MotionMarkAnimometer', () => {
+      const signatureIds = [
+        1708520, 1708521, 1708522, 1708523, 1708524, 1708525, 1708526, 1708527, 1708528,
+      ];
+      const parentHash = '9ad671fb568a5b3027af35b5d42fc6dd385f25ed';
+      fetchMock.get(`${signaturesUrl()}?parent_signature=${parentHash}`, WIN10_MMA_SUBTESTS);
+      fetchMock.get(subtestsPerfDataUrl(frameworkId, signatureIds), WIN10_MMA_DATA);
+
+      it('should find Windows 10 MotionMarkAnimometer pgo subtests data', async () => {
+        const data = await subbenchmarksData(frameworkId, platform, 'raptor-motionmark-animometer-firefox', 'pgo');
+        const modifiedExpectedData = downcastDatetimesToStrings(WIN10_MMA_EXPECTED_DATA);
+        assert.deepEqual(data, modifiedExpectedData);
+      });
     });
   });
 });
