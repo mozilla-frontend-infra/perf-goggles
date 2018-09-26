51b1240a7d7c6b7289a2ec463f2c191cc333f26c
Armen Zambrano G <armenzg@gmail.com>
2018-09-26 15:13:01 -0400

    Single function to handle benchmark & subtests data

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,9 @@
 /* global describe it */
 import fetchMock from 'fetch-mock';
 import {
-  fetchBenchmarkData,
   perfDataUrls,
   signaturesUrl,
-  subbenchmarksData,
+  queryPerformanceData,
   TREEHERDER,
 } from '../src';
 import MAC_STYLEBENCH_SIGNATURES from './mocks/mac/StyleBench/signatures';
@@ -29,9 +28,9 @@ const TIMERANGE = 3 * 24 * 3600;
 
 const downcastDatetimesToStrings = (data) => {
   const newData = Object.assign({}, data);
-  Object.keys(newData.data).forEach((node) => {
-    newData.data[node].data.forEach((datum, index) => {
-      newData.data[node].data[index].datetime = new Date(datum.push_timestamp * 1000);
+  Object.keys(newData).forEach((node) => {
+    newData[node].data.forEach((datum, index) => {
+      newData[node].data[index].datetime = new Date(datum.push_timestamp * 1000);
     });
   });
   return newData;
@@ -56,8 +55,8 @@ describe('Talos', () => {
       LINUX64_SIGNATURES,
     );
     describe('Jetstream', () => {
-      const { extraOptions, option, suite } = seriesConfig;
       const signatureIds = [
+        1661254, // The parental signature
         1661255, 1661256, 1661257, 1661258, 1661259, 1661260, 1661261, 1661262, 1661263, 1661264,
         1661265, 1661266, 1661267, 1661268, 1661269, 1661270, 1661271, 1661272, 1661273, 1661274,
         1661275, 1661276, 1661277, 1661278, 1661279, 1661280, 1661281, 1661282, 1661283, 1661284,
@@ -76,8 +75,7 @@ describe('Talos', () => {
       );
 
       it('should find Linux64 JetStream pgo subtests data', async () => {
-        const data =
-          await subbenchmarksData(frameworkId, platform, suite, option, extraOptions, TIMERANGE);
+        const data = await queryPerformanceData(seriesConfig, true, TIMERANGE);
         const modifiedExpectedData = downcastDatetimesToStrings(LINUX64_JETSTREAM_EXPECTED_DATA);
         assert.deepEqual(data, modifiedExpectedData);
       });
@@ -86,28 +84,29 @@ describe('Talos', () => {
 });
 
 describe('Raptor', () => {
-  const seriesConfig = {
-    frameworkId: 10,
-    option: 'pgo',
-    project: PROJECT,
-  };
-  const { frameworkId, option, project } = seriesConfig;
-
   describe('Windows 10', () => {
-    const platform = 'windows10-64';
+    const seriesConfig = {
+      frameworkId: 10,
+      option: 'pgo',
+      project: PROJECT,
+      platform: 'windows10-64',
+    };
+    const { frameworkId, platform, project } = seriesConfig;
     fetchMock.get(`${signaturesUrl(project)}?framework=${frameworkId}&platform=${platform}&subtests=0`, WIN10_SIGNATURES);
 
     describe('MotionMarkAnimometer', () => {
-      const suite = 'raptor-motionmark-animometer-firefox';
+      seriesConfig.suite = 'raptor-motionmark-animometer-firefox';
+      // 1708519 is the parental signature
       const signatureIds = [
-        1708520, 1708521, 1708522, 1708523, 1708524, 1708525, 1708526, 1708527, 1708528,
+        1708519, 1708520, 1708521, 1708522, 1708523, 1708524, 1708525, 1708526, 1708527, 1708528,
       ];
       const parentHash = '9ad671fb568a5b3027af35b5d42fc6dd385f25ed';
+
       fetchMock.get(`${signaturesUrl(project)}?parent_signature=${parentHash}`, WIN10_MMA_SUBTESTS);
       fetchMock.get(perfDataUrls(seriesConfig, signatureIds, TIMERANGE)[0], WIN10_MMA_DATA);
 
       it('should find Windows 10 MotionMarkAnimometer pgo subtests data', async () => {
-        const data = await subbenchmarksData(frameworkId, platform, suite, option, [], TIMERANGE);
+        const data = await queryPerformanceData(seriesConfig, true, TIMERANGE);
         const modifiedExpectedData = downcastDatetimesToStrings(WIN10_MMA_EXPECTED_DATA);
         assert.deepEqual(data, modifiedExpectedData);
       });
@@ -115,6 +114,11 @@ describe('Raptor', () => {
   });
 
   describe('Mac OS X', () => {
+    const seriesConfig = {
+      frameworkId: 10,
+      option: 'pgo',
+      project: PROJECT,
+    };
     describe('StyleBench', () => {
       const signatureIds = MAC_STYLEBENCH_SIGNATURES;
 
@@ -126,16 +130,22 @@ describe('Raptor', () => {
   });
 
   describe('Windows 7 32-bit', () => {
-    const platform = 'windows7-32';
+    const seriesConfig = {
+      frameworkId: 10,
+      option: 'opt',
+      project: PROJECT,
+      platform: 'windows7-32',
+    };
+    const { frameworkId, platform, project } = seriesConfig;
     fetchMock.get(`${signaturesUrl(project)}?framework=${frameworkId}&platform=${platform}&subtests=0`, WIN7_SIGNATURES);
 
     describe('MotionMarkAnimometer main score', () => {
       const suite = 'raptor-motionmark-animometer-firefox';
-
+      seriesConfig.suite = suite;
       fetchMock.get(perfDataUrls(seriesConfig, ['1713376'], TIMERANGE)[0], WIN7_MMA_DATA);
 
       it('The benchmark data should match', async () => {
-        const data = await fetchBenchmarkData(frameworkId, platform, suite, 'opt', [], TIMERANGE);
+        const data = await queryPerformanceData(seriesConfig, false, TIMERANGE);
         assert.deepEqual(data, downcastDatetimesToStrings(WIN7_MMA_EXPECTED_DATA));
       });
     });
