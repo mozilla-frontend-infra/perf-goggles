5be501638be210f6547d9470ce967bf2c172695a
Armen Zambrano G <armenzg@gmail.com>
2018-11-05 13:35:05 -0500

    Use camelcase naming for timerange

--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@ const platformSuitesUrl = ({ frameworkId, platform, project }) => (
 );
 
 export const perfDataUrls =
-  ({ frameworkId, project }, signatureIds, timerange) => {
+  ({ frameworkId, project }, signatureIds, timeRange) => {
     const url = dataPointsEndpointUrl(project);
     const baseParams = stringify({
       framework: frameworkId,
-      interval: timerange,
+      interval: timeRange,
     });
     // To guarantee order for tests
     signatureIds.sort();
@@ -46,9 +46,9 @@ const tranformData = data =>
 // The data contains an object where each key represents a subtest
 // Each data point of that subtest takes the form of:
 // {job_id: 162620134, signature_id: 1659462, id: 414057864, push_id: 306862, value: 54.89 }
-const fetchPerfData = async (seriesConfig, signatureIds, timerange) => {
+const fetchPerfData = async (seriesConfig, signatureIds, timeRange) => {
   const dataPoints = {};
-  await Promise.all(perfDataUrls(seriesConfig, signatureIds, timerange)
+  await Promise.all(perfDataUrls(seriesConfig, signatureIds, timeRange)
     .map(async (url) => {
       const data = await (await fetch(url)).json();
       Object.keys(data).forEach((hash) => {
@@ -62,8 +62,8 @@ const fetchPerfData = async (seriesConfig, signatureIds, timerange) => {
 };
 
 const perfherderGraphUrl =
-  ({ project = PROJECT, frameworkId }, signatureIds, timerange = DEFAULT_TIMERANGE) => {
-    let baseDataUrl = `${TREEHERDER}/perf.html#/graphs?timerange=${timerange}`;
+  ({ project = PROJECT, frameworkId }, signatureIds, timeRange = DEFAULT_TIMERANGE) => {
+    let baseDataUrl = `${TREEHERDER}/perf.html#/graphs?timerange=${timeRange}`;
     baseDataUrl += `&${signatureIds.sort().map(id =>
       `series=${project},${id},1,${frameworkId}`).join('&')}`;
     return baseDataUrl;
@@ -154,10 +154,10 @@ const parentSignatureInfo = async (seriesConfig) => {
   return findParentSignatureInfo(seriesConfig, signatures, options);
 };
 
-const fetchSubtestsData = async (seriesConfig, subtestsInfo, timerange) => {
+const fetchSubtestsData = async (seriesConfig, subtestsInfo, timeRange) => {
   const signatureIds = Object.values(subtestsInfo).map(v => v.id);
   const subtestsData = {};
-  const dataPoints = await fetchPerfData(seriesConfig, signatureIds, timerange);
+  const dataPoints = await fetchPerfData(seriesConfig, signatureIds, timeRange);
 
   Object.keys(dataPoints).forEach((subtestHash) => {
     subtestsData[subtestHash] = {
@@ -174,7 +174,7 @@ export const queryPerformanceData = async (
   seriesConfig,
   options,
 ) => {
-  const { includeSubtests = false, timerange = DEFAULT_TIMERANGE } = options;
+  const { includeSubtests = false, timeRange = DEFAULT_TIMERANGE } = options;
   const parentInfo = await parentSignatureInfo(seriesConfig);
   // XXX: Throw error instead
   if (!parentInfo) {
@@ -182,7 +182,7 @@ export const queryPerformanceData = async (
   }
   let perfData = {};
   if (!(includeSubtests && parentInfo.has_subtests)) {
-    const dataPoints = await fetchPerfData(seriesConfig, [parentInfo.id], timerange);
+    const dataPoints = await fetchPerfData(seriesConfig, [parentInfo.id], timeRange);
     perfData = {
       [parentInfo.parentSignatureHash]: {
         data: dataPoints[parentInfo.parentSignatureHash],
@@ -193,7 +193,7 @@ export const queryPerformanceData = async (
   } else {
     const subtestsMeta = await querySubtests(seriesConfig, parentInfo.parentSignatureHash);
     subtestsMeta[parentInfo.parentSignatureHash] = parentInfo;
-    const subtestsData = await fetchSubtestsData(seriesConfig, subtestsMeta, timerange);
+    const subtestsData = await fetchSubtestsData(seriesConfig, subtestsMeta, timeRange);
     Object.keys(subtestsData).forEach((hash) => { perfData[hash] = subtestsData[hash]; });
   }
 
